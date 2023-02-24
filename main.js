c5af72091945d0347f8c5b489ab5aad7598754bb
Rioba-Ian-Felix <riobafelix@gmail.com>
2023-02-24 22:38:36 +0300

    first commit --scaffold input form, styles 👍

---
+++ main.js
@@ -0,0 +1,19 @@
+let myLibrary = [
+  {
+    "book": "The Hobbit",
+    "author": "JK Rowlins",
+    "pages": 289,
+    "read": true
+  }
+]
+
+function Book() {
+  // constructor ..
+
+}
+
+function addBookToLibrary() {
+  // stuff here..
+}
+
+console.log(myLibrary)

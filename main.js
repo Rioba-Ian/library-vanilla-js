21c3e3c74cb88b2c0642826554bb804fc75d11b5
Rioba Ian <riobafelix@gmail.com>
2023-02-26 17:14:47 +0300

    display books onto the page

--- main.js
+++ main.js
@@ -7,6 +7,11 @@ let myLibrary = [
   }
 ]
 
+const form = document.querySelector("form")
+
+const bookCards = document.querySelector(".book-cards")
+
+
 function Book() {
   // constructor ..
 
@@ -14,30 +19,49 @@ function Book() {
 
 function addBookToLibrary() {
   // stuff here..
+
+  // add book to my library
+  form.addEventListener("submit", function (e) {
+    e.preventDefault()
+  
+    const formData = new FormData(form)
+  
+    const readVal = formData.get('read')
+    if (!readVal) {
+      formData.append('read', false)
+    }
+  
+    const formDataObj = Object.fromEntries(formData.entries())
+  
+    myLibrary = [...myLibrary, formDataObj]
+    console.log(formDataObj)
+    console.log(myLibrary)
+  
+    form.reset()
+    closeModal()
+
+  })
 }
 
+function addBookToDisplay(){
+  myLibrary.forEach((bookItem)=> {
+    const card = document.createElement("div")
+    card.classList.add("card")
+    card.innerHTML = `<p>${bookItem.book}</p>
+                      <p>${bookItem.author}</p>
+                      <p>${bookItem.book}</p>
+                      <p>${bookItem.read == true? "Read": "Not yet read."}</p>
+                      `
+  
+    bookCards.appendChild(card)
+  })
+}
 
-const form = document.querySelector("form")
+addBookToLibrary()
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault()
+addBookToDisplay()
 
-  const formData = new FormData(form)
 
-  const readVal = formData.get('read')
-  if (!readVal) {
-    formData.append('read', false)
-  }
-
-  const formDataObj = Object.fromEntries(formData.entries())
-
-  myLibrary = [...myLibrary, formDataObj]
-  console.log(formDataObj)
-  console.log(myLibrary)
-
-  form.reset()
-  closeModal()
-})
 
 
 // modal functionality
@@ -65,3 +89,8 @@ function closeModal() {
   modal.classList.add("hidden")
   overlay.classList.add("hidden")
 }
+
+// add content into the card
+
+
+
